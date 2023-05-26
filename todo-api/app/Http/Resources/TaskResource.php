d535600de1f595acfeab491bb5ce4805bcb49085
mayanksdudakiya <mayanksdudakiya@gmail.com>
2023-05-26 16:00:11 +0530

    Task resource has added

---
+++ todo-api/app/Http/Resources/TaskResource.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TaskResource extends JsonResource
+{
+    /**
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'title'        => $this->title,
+            'is_completed' => $this->is_completed,
+        ];
+    }
+}
