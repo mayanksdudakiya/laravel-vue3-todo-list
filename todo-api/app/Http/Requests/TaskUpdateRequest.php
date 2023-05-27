c1f3fd2811871c93c6ebf861c2e8c7e0f835b6cb
mayanksdudakiya <mayanksdudakiya@gmail.com>
2023-05-28 01:39:02 +0530

    Task update request has added

---
+++ todo-api/app/Http/Requests/TaskUpdateRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TaskUpdateRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title'        => ['required', 'string', 'min:5', 'max:255'],
+            'is_completed' => ['sometimes', 'required', 'boolean'],
+        ];
+    }
+}
