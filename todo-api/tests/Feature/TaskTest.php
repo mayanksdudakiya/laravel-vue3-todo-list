328030590a69966ca28fec2ee15cc490e37ffdf1
mayanksdudakiya <mayanksdudakiya@gmail.com>
2023-05-27 23:33:01 +0530

    Task index test has added

--- todo-api/tests/Feature/TaskTest.php
+++ todo-api/tests/Feature/TaskTest.php
@@ -2,12 +2,14 @@
 
 namespace Tests\Feature;
 
+use App\Models\Task;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class TaskTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function test_tasks_screen_should_be_rendereed(): void
     {
         $response = $this->get(route('tasks.index'));
@@ -66,5 +68,25 @@ class TaskTest extends TestCase
 
         $this->post(route('tasks.store'), $newTask)
             ->assertStatus(422);
+    }
+
+    public function test_tasks_can_be_indexed(): void
+    {
+        Task::factory(10)->create();
+
+        $this->get(route('tasks.index'))
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => [
+                    '*' => [
+                        'id',
+                        'title',
+                        'is_completed',
+                        'created_at',
+                        'updated_at',
+                    ]
+                ]
+            ])
+            ->assertJsonCount(10, 'data');
     }
 }
