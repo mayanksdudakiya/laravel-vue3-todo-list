760d22768cbe1cc60c9959c4ec5aaa0903c54672
mayanksdudakiya <mayanksdudakiya@gmail.com>
2023-06-02 02:35:32 +0530

    Route resource replaced with apiResource

--- todo-api/routes/api.php
+++ todo-api/routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('tasks', TaskController::class)->except(['create', 'edit']);
+Route::apiResource('tasks', TaskController::class);
