63b0e7d3dab038df997d12a5f558f9eda09ccc9c
mayanksdudakiya <mayanksdudakiya@gmail.com>
2023-06-02 02:37:01 +0530

    Fixed view name in index.js

--- todo-app/src/router/index.js
+++ todo-app/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import TodoView from '../views/TodoView.vue'
+import TaskView from '../views/TaskView.vue'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +7,7 @@ const router = createRouter({
         {
             path: '/',
             name: 'home',
-            component: TodoView
+            component: TaskView
         },
     ]
 })
